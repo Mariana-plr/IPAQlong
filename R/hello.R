5ca54235a4ecace724461137cab044ef2eff4bce
Mariana-plr <79210610+Mariana-plr@users.noreply.github.com>
2022-03-18 14:21:46 +0100

    first commit

---
+++ R/hello.R
@@ -0,0 +1,28 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+library(roxygen2) # In-Line Documentation for R
+library(devtools) # Tools to Make Developing R Packages Easier
+library(testthat) # Unit Testing for R
+library(usethis) # Automate Package and Project Setup
+library(available)
+
+git remote add origin https://github.com/YOUR_USERNAME/REPOSITORY_NAME.git
+git commit -m "initial commit"
+git push -u origin master
